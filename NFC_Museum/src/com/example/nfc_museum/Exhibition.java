da438f87e018d609b0a25f9b6d24a8bc5adf0c23
jeontaebo <inthewinter11@gmail.com>
2013-08-12 13:26:00 +0900

    dialogbox is added

--- NFC_Museum/src/com/example/nfc_museum/Exhibition.java
+++ NFC_Museum/src/com/example/nfc_museum/Exhibition.java
@@ -1,15 +1,20 @@
 package com.example.nfc_museum;
 
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Spinner;
-import android.widget.Toast;
-import android.app.Activity;
-import android.content.Intent;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 
@@ -21,6 +26,31 @@ public class Exhibition extends Activity{
 	private Button texture_btn;
 	private Button use_btn;
 	private Button design_btn;	
+	
+	private static final int DIALOG_YES_OR_NO_MESSAGE=1;
+	String country[];
+	
+	//국가 선택 대화 상자 추가
+	@Override
+	protected Dialog onCreateDialog(int id){
+		switch(id){
+		case DIALOG_YES_OR_NO_MESSAGE:
+			final CharSequence[] items = {"China","Korea","Japan"};
+			
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Select contry");
+			builder.setItems(items, new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog,int item){
+					Intent in = new Intent(Exhibition.this,Exhibit_list.class);
+					in.putExtra("country",items[item]);
+					startActivity(in);
+				}
+			});
+			AlertDialog alert = builder.create();
+			return alert;
+		}
+		return null;
+	}
 	
 	
 	@Override
@@ -57,8 +87,7 @@ public class Exhibition extends Activity{
 	
 		country_btn.setOnClickListener(new OnClickListener(){
 			public void onClick(View v){
-				Intent intent = new Intent(Exhibition.this, Exhibit_list.class);
-				startActivity(intent);
+				showDialog(DIALOG_YES_OR_NO_MESSAGE); //버튼 클릭시 대화 상자 뜸
 			}
 		});
 		

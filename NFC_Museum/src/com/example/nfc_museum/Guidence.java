ee37a16e7600eb4828fdf67031627e2233252b8c
tpghk1063 <sehwa@psh>
2013-08-14 15:17:41 +0900

    소스 합친것

---
+++ NFC_Museum/src/com/example/nfc_museum/Guidence.java
@@ -0,0 +1,15 @@
+package com.example.nfc_museum;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Guidence extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.guidence);
+		
+	}
+}
---
+++ NFC_Museum/src/com/example/nfc_museum/History.java
@@ -0,0 +1,15 @@
+package com.example.nfc_museum;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class History extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.history);
+		
+	}
+}
